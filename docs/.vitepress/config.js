2de581e9150f6e1fc072c6d61be4eb343bd9fce4
patrick.ryanto@outlook.com <patrick.ryanto@outlook.com>
2022-08-29 16:40:13 +0700

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,122 @@
+import { defineConfig } from "vitepress"
+
+export default defineConfig({
+  lang: 'id-ID',
+  title: 'Klearance Online',
+  description: 'Aplikasi Pelaporan Bea Cukai',
+  head:[['link', { rel: "shortcut icon", href: "/klearancelogo.ico"}]],
+
+  lastUpdated: true,
+  locales: {
+    '/': {
+      lang: 'id-ID',
+      selectText: 'Bahasa Indonesia'
+    },
+    '/en/': {
+      lang: 'en-US',
+      selectText: 'English'
+    }
+  },
+  themeConfig: {
+    logo: '/klearancelogo.png',
+    siteTitle: false,
+        localeLinks: {
+      text: '',
+      items: [
+        { text: 'English', link: '/en/' },
+        { text: 'Bahasa Indonesia', link: '/' }
+      ]
+    },
+    nav: nav(),
+    sidebar: {
+      '/overview/': [
+        {
+          text: 'Overview',
+          items: [
+            { text: 'Klearance', link: '/overview/klearance' }, 
+            { text: 'Arsitektur', link: '/overview/arsitektur' }, 
+            { text: 'Langkah Awal', link: '/overview/langkahawal' }, 
+          ]
+        },
+        {
+          text: 'Administrasi',
+          items: [
+            { text: 'User', link: '/overview/user' }, 
+            { text: 'Group', link: '/overview/langkahawal.html#group-akses' }, 
+            { text: 'Format Dokumen', link: '/overview/formatdokumen' }, 
+            { text: 'Import Mapping', link: '/overview/importmapping' }, 
+            { text: 'Dokumen Wajib Vendor', link: '/overview/dokumenwajibvendor' }, 
+          ]
+        },
+        {
+          text: 'Integrasi',
+          items: [
+            { text: 'API', link: '/overview/api' }, 
+          ]
+        }
+      ],
+      '/klrkb/': [
+        {
+          text: 'Langkah Awal',
+          items: [
+            { text: 'Mengundang Vendor', link: '/klrkb/mengundangvendorkb/' }, 
+          ]
+        },
+        {
+          text: 'Dokumen',
+          items: [
+            { text: 'Kepabeanan', link: '/klrkb/dokumenbc/' }, 
+            { text: 'Internal', link: '/klrkb/dokumeninternal/' }, 
+          ]
+        },
+        {
+          text: 'Integrasi',
+          items: [
+            { text: 'Sync Client', link: '/klrkb/syncclient/' }, 
+            { text: 'TPB CEISA', link: '/klrkb/tpbceisa/' }, 
+            { text: 'CDMS KB', link: '/klrkb/cdmskb/' }, 
+          ]
+        }
+      ],
+
+      '/klriuip': [
+        {
+          text: 'Langkah Awal',
+          items: [
+            { text: 'Mengundang Vendor', link: '/klriuip/mengundangvendoriuip/' }, 
+            { text: 'Eksternal', link: '/klriuip/usereksternal/' }, 
+          ]
+        },
+        {
+          text: 'Dokumen',
+          items: [
+            { text: 'Kepabeanan', link: '/klriuip/dokumenbc/' }, 
+            { text: 'Internal', link: '/klriuip/dokumeninternal/' }, 
+            { text: 'Eksternal', link: '/klriuip/dokumeneksternal/' }, 
+          ]
+        },
+        {
+          text: 'Integrasi',
+          items: [
+            { text: 'Sync Client', link: '/klriuip/syncclient/' }, 
+            { text: 'PIB', link: '/klriuip/pib/' }, 
+          ]
+        }
+      ]
+    },
+    footer: {
+      message: 'Bea Cukai Lebih Baik',
+      copyright: 'Copyright © 2022 PT Solusi Manufaktur Teknologi'
+    }
+  }
+})
+
+function nav() {
+  return [
+    { text: 'Overview', link: '/overview/klearance', activeMatch: '/overview/klearance' },
+    { text: 'Klearance KB', link: '/klrkb/mengundangvendorkb/', activeMatch: '/klrkb' },
+    { text: 'Klearance IU/IP', link: '/klriuip/mengundangvendoriuip/', activeMatch: '/klriuip' },
+    { text: 'Term of Services', link: '/legal/tos', activeMatch: '/legal/tos' },
+  ]
+}
+
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+---
+layout: home
+hero:
+  name: Aplikasi pelaporan dokumen Bea Cukai
+
+  tagline: Data terjamin aman, pengoperasian mudah, Customs Clearance dapat dilakukan di mana saja, kapan saja
+  actions:
+    - theme: brand
+      text: Mulai Belajar
+      link: /overview/klearance
+features:
+  - icon: ⚡️
+    title: Integrasi Cepat
+    details: Dengan kemampuan komputer yang minimal Anda dapat mengikuti panduan ini untuk melakukan integrasi
+  - icon: 📊
+    title: Kontrol Data Akurat
+    details: Data pelaporan Kepabeanan lebih dapat dipertanggungjawabkan 
+  - icon: 🛠️
+    title: Customizable
+    details: Tim SMART (https://smartconsultant.co) selalu siap membantu jika kustomisasi dibutuhkan oleh perusahaan Anda
+---
